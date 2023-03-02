03ba01c2c7f374e64de94c2e717efc16455d45e2
Стас Басов <stas.basov@whatever.com>
2023-03-02 23:14:56 +0300

    commits

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str,
            char_class: str) -> str:
+    """Урон персонажа."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -16,6 +19,7 @@ def attack(char_name: str,
 
 def defence(char_name: str,
             char_class: str) -> str:
+    """Защита персонажа."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -26,6 +30,7 @@ def defence(char_name: str,
 
 def special(char_name: str,
             char_class: str) -> str:
+    """Спец атаки персонажей."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -37,6 +42,7 @@ def special(char_name: str,
 
 def start_training(char_name: str,
                    char_class: str) -> str:
+    """Начало тренировки персонажей."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +68,7 @@ def start_training(char_name: str,
 
 
 def choice_char_class() -> str:
+    """Выбор персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -83,7 +90,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
